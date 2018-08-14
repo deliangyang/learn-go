c560cb717fdfa6281904bec1b1426ae8430a9856
ydl <yangdeliang@juwang.cn>
2018-08-14 12:17:41 +0800

    stack length

--- strcut/stack.go
+++ strcut/stack.go
@@ -15,18 +15,28 @@ type Node struct {
 
 type Stack struct {
 	node *Node
+	length int
 }
 
 type Key interface {}
 
 func NewStack() *Stack {
-	s := &Stack{nil}
+	s := &Stack{nil, 0}
 	return s
 }
 
 func (stack *Stack) push(key Key) {
 	s := &Node{key: key, next:stack.node}
+	stack.length++
 	stack.node = s
+}
+
+func (stack *Stack) len() int  {
+	return stack.length
+}
+
+func (stack *Stack) isEmpty() bool {
+	return stack.length == 0
 }
 
 func (stack *Stack) pop()(interface{}, bool) {
@@ -35,6 +45,7 @@ func (stack *Stack) pop()(interface{}, bool) {
 		return nil, false
 	}
 	stack.node = stack.node.next
+	stack.length--
 	return node.key, true
 }
 
@@ -43,10 +54,14 @@ func main() {
 
 	stack.push(1)
 	stack.push(2)
+	fmt.Println("length:", stack.len())
 	key, _ := stack.pop()
 	fmt.Println(key)
+	fmt.Println(stack.len())
+	fmt.Println("is empty:", stack.isEmpty())
 	key2, _ := stack.pop()
 	fmt.Println(key2)
+	fmt.Println("is empty:", stack.isEmpty())
 	key1, _ := stack.pop()
 	fmt.Println(key1)
 
